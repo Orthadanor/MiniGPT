9abe384898c27a1e3f374e5e03cc5aab12cd6d66
QiudiHe <130117975+qdh-2002@users.noreply.github.com>
2025-05-19 21:22:24 -0700

    Update config.py

--- config.py
+++ config.py
@@ -54,3 +54,38 @@ class MiniGPTConfig:
     gradient_clip: float = 1.0
     scheduler: bool = False
     parallelHeads: bool = False # Parallelized multi-head attention for faster computation
+
+
+@dataclass
+class MiniTransformerEncoderConfig:
+    path_to_data: Path = Path("data")
+    batch_size: int = 32
+    num_layers: int = 1  # Num of Transformer layers
+    vocab_size: int = 50257  # Tiktoken for GPT2 size
+    embed_dim: int = (
+        64  # Dimensionality of the the token embeddings (throught the transformer)
+    )
+    feedforward_size: Optional[int] = (
+        None  # hidden layer in the feedforward network, None sets it to 4*embed_dim
+    )
+
+    context_length: int = 10  # Max number of tokens in a sequence
+    num_heads: int = 4  # Number of heads in the multihead attention
+    weight_tie: bool = (
+        True  # Whether to tie the weights of the embedding and the output layer
+    )
+    feedforward_dropout: float = 0.1  # Dropout in the feedforward layer
+    attention_dropout: float = 0.1  # Dropout in the attention layer
+    dropout: float = 0.1  # Dropout in the output layer
+    embed_dropout: float = 0.1  # Dropout in the embedding layer
+    learning_rate: float = 3e-4  # Learning rate for the optimizer
+    log_interval: int = 1000
+    save_path: Path = Path("models/minitransformer/")
+    save_iterations: int = 2000
+    to_log: bool = True
+    max_iter: int = 100000
+    to_clip_grad: bool = False
+    gradient_clip: float = 1.0
+    scheduler: bool = False
+
+
